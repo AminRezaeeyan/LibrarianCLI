06a0174b34ed79a1d009918f4ef75160f5e72d3d
Amin Rezaeeyan <rezaeeyanamin@gmail.com>
2024-08-04 16:47:52 +0330

    feat: add Main class

---
+++ src/main/java/com/librarian/Main.java
@@ -0,0 +1,9 @@
+package com.librarian;
+
+import com.librarian.CLI.CommandLineInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        CommandLineInterface.getInstance().start();
+    }
+}
